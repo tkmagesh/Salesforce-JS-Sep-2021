aa195b9cfba7dd5bb77b2beaa1b3b40ddf435075
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-08 12:09:07 +0530

    sum example complete solution

--- 01-language/src/calculator.js
+++ 01-language/src/calculator.js
@@ -1,18 +1,8 @@
 function sum(){
     function parseArg(n){
         if (typeof n === 'function') return parseArg(n());
-        if (Array.isArray(n)) {
-            var result = 0;
-            for (var index = 0; index < n.length; index++) {
-                result += parseArg(n[index]);
-            }
-            return result;
-        }
+        if (Array.isArray(n)) return sum.apply(this, n);
         return isNaN(n) ? 0 : parseInt(n);
     }
-    var result = 0;
-    for(var index = 0; index < arguments.length; index++){
-        result += parseArg(arguments[index]);
-    }
-    return result;
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + sum([].slice.call(arguments, 1));
 }
