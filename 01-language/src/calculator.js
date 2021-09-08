74370036d8e922c6485d692e16561d0cd5027a16
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-08 13:12:25 +0530

    clickTracker

--- 01-language/src/calculator.js
+++ 01-language/src/calculator.js
@@ -5,4 +5,11 @@ function sum(){
         return isNaN(n) ? 0 : parseInt(n);
     }
     return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + sum([].slice.call(arguments, 1));
+    /* 
+        var parsed = [].map.call(arguments, parseArg);
+        return parsed.reduce((x,y) => x + y, 0) 
+    */
+    /* 
+        [].map.call(arguments, parseArg).reduce((x,y) => x + y, 0); 
+    */
 }
