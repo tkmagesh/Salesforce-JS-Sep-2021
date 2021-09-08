791d312044ff505f7836c0acbdaef532e60ebe5d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-08 11:03:45 +0530

    sum function implementation - Version - 1

--- 01-language/src/calculator.js
+++ 01-language/src/calculator.js
@@ -1,3 +1,18 @@
-function sum(x,y){
-    return x + y;
+function sum(){
+    function parseArg(n){
+        if (typeof n === 'function') return parseArg(n());
+        if (Array.isArray(n)) {
+            var result = 0;
+            for (var index = 0; index < n.length; index++) {
+                result += parseArg(n[index]);
+            }
+            return result;
+        }
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    var result = 0;
+    for(var index = 0; index < arguments.length; index++){
+        result += parseArg(arguments[index]);
+    }
+    return result;
 }
