abc735405ca13170d4e076b9e1e743a772360e30
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-09 09:55:44 +0530

    es6 features - part -1

--- 03-constructor-functions/spinner.js
+++ 03-constructor-functions/spinner.js
@@ -1,3 +1,6 @@
+/* 
+    we get privacy at the cost of memory
+*/
 function Spinner() {
     var counter = 0;
 
@@ -9,3 +12,17 @@ function Spinner() {
         return --counter;
     }
 }
+
+
+/* We get memory efficiency at the cost of privacy */
+function Spinner() {
+    this.__counter__ = 0;
+}
+
+Spinner.prototype.increment = function () {
+    return ++this.__counter__;
+};
+
+Spinner.prototype.decrement = function() {
+    return --this.__counter__;
+};
---
+++ 04-es6/es6.js
@@ -0,0 +1,63 @@
+/* 
+Features
+    1. let
+    2. const
+    3. Array destructuring
+*/
+
+//var => is NOT block scoped
+
+//01. let
+for(let i=0; i<10; i++){
+}
+
+//02. const
+const PI = 3.14;
+
+//03. Array destructuring
+var nos = [3,1,4,2,5]
+var [x, y] = nos
+[x,y] = [y,x]
+
+//04. Rest operator (Array)
+var [x,y, ...z] = nos
+
+//05. Spread operator (Array)
+var newNos = [ ...nos, 10, 20, 30 ]
+function add(x,y){
+    return x + y;
+}
+add(...nos)
+
+
+//06. Object destructuring
+var emp = {
+    id : 100,
+    name : 'Magesh',
+    salary : 10000
+}
+
+var { salary, id } = emp
+var { salary : x, id : y } = emp
+
+//07. Rest operator (Object)
+var { id : x, ...z } = emp
+
+//08. Spread operator (Object)
+var newEmp = { ...emp, city : 'Bengaluru' }
+
+var emp = {
+    id : 100,
+    name : 'Magesh',
+    salary : 10000,
+    org : {
+        name : 'Salesforce',
+        city : 'Bengaluru'
+    }
+}
+
+var { id :x, org : { name, city} } = emp
+
+//09. Object construction enhancement
+var id = 101, name = 'Pen', cost = 10
+var product = { id , name , cost  }
